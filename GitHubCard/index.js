9a03b46fa07b91e93ee7ffe4b1d494b876dc142a
MarkelWhit <litslayer@gmail.com>
2021-06-03 18:42:04 -0500

    Working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get(`https://api.github.com/users/${users}`)
+.then(res => {
+  console.log(res.data)
+  
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -32,23 +39,63 @@ const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+   Using DOM methods and properties, create and return the following markup:*/
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
+const cards = document.querySelector("div.cards");
+function cardMaker({ avatar_url, name, login, location, html_url, followers, following, bio }) {
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const cardName = document.createElement("h3");
+  const uName = document.createElement("p");
+  const uLocation = document.createElement("p");
+  const profile = document.createElement("p");
+  const address = document.createElementNS("a");
+  const cardFollow = document.createElement("p");
+  const cardFollowing = document.createElement("p");
+  const cardBio = document.createElement("p");
+  
+  image.src = avatar_url
+  cardName.textContent = name
+  uName.textContent = login
+  uLocation.textContent = locaion
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(uName);
+  cardInfo.appendChild(uLocation);
+  cardInfo.appendChild(profile);
+  profile.appendChild(address);
+  cardInfo.appendChild(cardFollow);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+
+  card.classList.add("card");
+  card.classList.add("card-info");
+  cardInfo.classList.add("name");
+  cardInfo.classList.add("username");
+return card;
+}
+
+cards.appendChild(cardMaker({avatar_url, name, login, location, html_url, followers, following, bio}))
+
+/*<div class="card">
+  <img src={image url of user} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div>
 */
+
 
 /*
   List of LS Instructors Github username's:
